a7afb87f259e8eb35d9aac5aadd51a4d218e44fb
mikol <mikolajek.a@gmail.com>
2023-07-25 18:34:11 +0200

    module 189 - turning Streams into Widgets USing StreamBuilder (StreamBuilder added)

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -70,6 +70,33 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>(
+              stream: _firestore.collection('messages').snapshots(),
+              builder: (context, snapshot) {
+                List<Text> messageWidgets = [];
+                if (!snapshot.hasData) {
+                  return Center(child: CircularProgressIndicator(backgroundColor: Colors.lightBlueAccent,),);
+                }
+                final messages = snapshot.data!.docs;
+
+                for (var message in messages) {
+                final messageText = message['text'];
+                final messageSender = message['sender'];
+                final messageWidget =
+                Text('$messageSender said $messageText');
+                messageWidgets.add(messageWidget);
+                }
+                return Column(
+                children: messageWidgets,
+                );
+                //  else {
+                //   return Container(
+                //     alignment: Alignment.center,
+                //     child: Text('No data available.'),
+                //   );
+                // }
+                },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
